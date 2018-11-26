16850646313e384452a567682d5e5926a5b7e679
Rshivapu <rohitshivapur18@gmail.com>
2018-11-26 08:44:33 -0800

    Adding a base mapping declarative

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import sqlalchemy
-print(sqlalchemy.__version__)
+# print(sqlalchemy.__version__)
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+db_engine = create_engine('sqlite:///:memory:', echo = False)
+Base = declarative_base()
